bf008e58ed6834f16503caf6f5842251c3159fd3
Your Name <damvaru4@gmail.com>
2020-03-17 13:49:03 -0500

    fizzbuzz

--- palindrome/fizzbuzz/index.js
+++ palindrome/fizzbuzz/index.js
@@ -17,7 +17,13 @@ function fizzBuzz(n) {
         // is the number a multiple of 3 and 5?
         if ( i % 3 === 0 && i % 5 === 0) {
             console.log('fizzbuzz');
-
+        } else if (i % 3 === 0){
+            // is the number a multiple of 3?
+            console.log('fizz');
+        } else if (i & 5 === 0){
+            console.log('buzz')
+        } else {
+            console.log(i);
         }
     }
 }
