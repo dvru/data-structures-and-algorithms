6403216371743f9c5750f2d0d1a42e9667365b0f
Your Name <damvaru4@gmail.com>
2020-03-23 20:29:53 -0500

    chunk

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,7 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+
 function chunk(array, size) {
     const chunked = [];
 
@@ -24,6 +25,7 @@ function chunk(array, size) {
 }
 
 module.exports = chunk;
+
 
 
 
