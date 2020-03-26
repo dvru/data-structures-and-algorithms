a3e3071a40e599fa9383f3fb626afd9231e82f42
Your Name <damvaru4@gmail.com>
2020-03-26 13:05:40 -0500

    anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,33 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
 function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA);  // produce char map for str a & b
-    const bCharMap = buildCharMap(stringB); 
 
-    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {  // if acharmap is not equal to bcharmap, return false
-        return false;
-    }
-
-    for (let char in aCharMap){   // look through a char map, 
-        if(aCharMap[char] !== bCharMap[char]){  // if they dont equal eachother,
-            return false;
-        }
-    }
-
-    return true;
 }
 
-// helper function
-function buildCharMap(str) {
-    const charMap = {};  // empty object serve as our character map
 
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {   // iterate through str,
-        charMap[char] = charMap[char] + 1 || 1;                   // and for every char, add to char map
-    }
 
-    return charMap;
-}
+
+
+
+
+// Solution 1
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);  // produce char map for str a & b
+//     const bCharMap = buildCharMap(stringB); 
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {  // if acharmap is not equal to bcharmap, return false
+//         return false;
+//     }
+
+//     for (let char in aCharMap){   // look through a char map, 
+//         if(aCharMap[char] !== bCharMap[char]){  // if they dont equal eachother,
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// // helper function
+// function buildCharMap(str) {
+//     const charMap = {};  // empty object serve as our character map
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {   // iterate through str,
+//         charMap[char] = charMap[char] + 1 || 1;                   // and for every char, add to char map
+//     }
+
+//     return charMap;
+// }
 
 
 module.exports = anagrams;
