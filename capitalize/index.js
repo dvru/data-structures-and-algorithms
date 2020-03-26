05930bbc8f304f7274d63072e012a88c697225cb
Your Name <damvaru4@gmail.com>
2020-03-26 13:15:25 -0500

    capitalize

--- capitalize/index.js
+++ capitalize/index.js
@@ -2,6 +2,7 @@
 // Write a function that accepts a string.  The function should
 // capitalize the first letter of each word in the string then
 // return the capitalized string.
+
 // --- Examples
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
