ceadec7476ad5477163f705b786cd788442dbf88
dvru <damvaru4@gmail.com>
2020-05-02 16:58:08 -0500

    Rule 3

---
+++ z-t-m Data-Structures/Rule3.js
@@ -0,0 +1,14 @@
+// Rule 3 : Different inputs should have Different variables
+
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function(boxes) {
+        console.log(boxes);
+    });
+
+    boxes2.forEach(function(boxes) {
+        console.log(boxes);
+    })
+}
+
+// 0(2n)
+0(n)
