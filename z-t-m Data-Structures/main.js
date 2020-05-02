a5ca236d814d37502c01831f84b110b0434979c0
dvru <damvaru4@gmail.com>
2020-05-02 15:36:26 -0500

    big O

---
+++ z-t-m Data-Structures/main.js
@@ -0,0 +1,19 @@
+const {performance} = require('perf_hooks');
+
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(10).fill('nemo');
+
+
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === 'nemo') {
+      console.log('Found Nemo');
+    }
+  }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took ' + (t1-t0) + ' milliseconds');
+}
+
+findNemo(large)
