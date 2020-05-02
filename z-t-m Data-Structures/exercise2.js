86be468b72ae3bb98de36564886bbb971e120f53
dvru <damvaru4@gmail.com>
2020-05-02 16:15:45 -0500

    Big 0 exercise2

---
+++ z-t-m Data-Structures/exercise2.js
@@ -0,0 +1,20 @@
+function anotherFunChallenge(input) {
+    let a = 5; // 0(1)
+    let b = 10; // 0(1)
+    let c = 50; // 0(1)
+    for (let i = 0; i < input; i++) { // 0(n)
+      let x = i + 1; // 0(1)
+      let y = i + 2; // 0(1)
+      let z = i + 3; // 0(1)
+  
+    }
+    for (let j = 0; j < input; j++) { // 0(n)
+      let p = j * 2; // 0(1)
+      let q = j * 2; // 0(1)
+    }
+    let whoAmI = "I don't know"; // 0(1)
+  }
+
+// 0(n)
+
+// 0(1)
