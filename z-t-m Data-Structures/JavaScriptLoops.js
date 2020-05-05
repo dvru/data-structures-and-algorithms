7f4b918143f9869e6e4196375353528ae31d400d
dvru <damvaru4@gmail.com>
2020-05-05 13:46:08 -0500

    LOOPS

---
+++ z-t-m Data-Structures/JavaScriptLoops.js
@@ -0,0 +1,22 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(10).fill('nemo');
+
+const findNemo2 = array => {
+    array.forEach(fish => {
+        if(fish === 'nemo') {
+            console.log('Found NEMO!')
+        }
+    })
+}
+
+const findNemo3 = array => {
+    for (let fish of array) {
+        if(fish === 'nemo') {
+            console.log('Found NEMO!');
+        }
+    }
+}
+
+findNemo2(everyone);
+findNemo3(everyone);
