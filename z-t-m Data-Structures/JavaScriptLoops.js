bf2c6e6d450916c3942a4c40c9220f0138fc98ab
dvru <damvaru4@gmail.com>
2020-05-05 13:46:46 -0500

    LOOPS

--- z-t-m Data-Structures/JavaScriptLoops.js
+++ z-t-m Data-Structures/JavaScriptLoops.js
@@ -3,8 +3,8 @@ const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', '
 const large = new Array(10).fill('nemo');
 
 const findNemo2 = array => {
-    array.forEach(fish => {
-        if(fish === 'nemo') {
+    array.forEach(name => {
+        if(name === 'nemo') {
             console.log('Found NEMO!')
         }
     })
