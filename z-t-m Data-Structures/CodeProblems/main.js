e98778a6d3cbcf524810b766bfd5e1ad33bf10f7
dvru <damvaru4@gmail.com>
2020-05-05 16:45:12 -0500

    added interview code

--- z-t-m Data-Structures/CodeProblems/main.js
+++ z-t-m Data-Structures/CodeProblems/main.js
@@ -27,3 +27,32 @@ function containsCommonItem(arr1, arr2) {
   }
 
   containsCommonItem(array1, array2)
+//O(a*b)
+//O(1) - Space Complexity
+
+
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'a'];
+
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create object where properties === items in the array
+  // can we assume always 2 params?
+
+  let map = {};
+  for (let i=0; i < arr1.length; i++) {
+    if(!map[arr1[i]]) {
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  // loop through second array and check if item in second array exists on created object. 
+  for (let j=0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
+    }
+  }
+  return false
+}
+
+//O(a + b) Time Complexity
+//O(a) Space Complexity
