3a2d072e4402ea4629c64168ddaa33d28ac86848
dvru <damvaru4@gmail.com>
2020-05-06 15:43:03 -0500

    merge arrays

--- z-t-m Data-Structures/Arrays/stringExercise2.js
+++ z-t-m Data-Structures/Arrays/stringExercise2.js
@@ -3,12 +3,23 @@
 
 function mergeSortedArrays(array1, array2) {
     const mergedArray = [];
-    let array1Item = array[0];
+    let array1Item = array1[0];
     let array2Item = array2[0];
 
     // Check input
-    
+    if (array1.length === 0){
+        return array2;
+    }
+    if (array2.length === 0){
+        return array2;
+    }
 
+    while (array1Item || array2Item) {
+        if(array1Item < array2Item) {
+            mergedArray.push(array1Item)
+            array1Item = array1[1]
+        }
+    }
     return mergedArray;
 }
 
