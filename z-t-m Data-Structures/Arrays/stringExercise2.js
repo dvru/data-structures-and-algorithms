2ea8386f4f9b9124f0284bf385c4ffe57b83c507
dvru <damvaru4@gmail.com>
2020-05-06 15:39:24 -0500

    merge arrays

---
+++ z-t-m Data-Structures/Arrays/stringExercise2.js
@@ -0,0 +1,15 @@
+// mergeSortedArrays([0,3,4,31], [4,6,30]);
+// [0,3,4,4,6,30,31]
+
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array[0];
+    let array2Item = array2[0];
+
+    // Check input
+    
+
+    return mergedArray;
+}
+
+mergeSortedArrays([0,3,4,31], [4,6,30]);
