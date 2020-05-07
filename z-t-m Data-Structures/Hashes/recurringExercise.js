6aa6d797014e69aabf11dd1f67432ee657ca26ab
dvru <damvaru4@gmail.com>
2020-05-07 15:09:33 -0500

    First recurring Exercise

--- z-t-m Data-Structures/Hashes/recurringExercise.js
+++ z-t-m Data-Structures/Hashes/recurringExercise.js
@@ -1,5 +1,5 @@
 //Google Question
-//Given an array = [2,5,1,2,3,5,1,2,4]:
+// Given an array = [2,5,1,2,3,5,1,2,4]
 //It should return 2
 
 //Given an array = [2,1,1,2,3,5,1,2,4]:
@@ -10,7 +10,35 @@
 
 
 function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i]=== input[j]){
+                return input[i]
+            }
+        }
+    }
+    return undefined;
+} // 0(n^2)
+
+function firstRecurringCharacter2(input) {
+     let map = {};
+     for (let i = 0; i < input.length; i++) {
+         if(map[input[i]] !== undefined) {
+             return input[i]
+         } else {
+             map[input[i]] = i
+         }
+         console.log(map)
+     }
+     return undefined
 }
+
+
+
+
+console.log(firstRecurringCharacter2([2,5,1,2,3,5,1,2,4]))
+// console.log(firstRecurringCharacter([1,2]))
+
 
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]
