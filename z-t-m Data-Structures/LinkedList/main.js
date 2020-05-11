d8e66d221d0fb1d38818254860fddafaf44a8837
dvru <damvaru4@gmail.com>
2020-05-11 13:41:36 -0500

    pointers

--- z-t-m Data-Structures/LinkedList/main.js
+++ z-t-m Data-Structures/LinkedList/main.js
@@ -1,8 +1,18 @@
-const basket = ['apples', 'grapes', 'pears']
+// const basket = ['apples', 'grapes', 'pears']
 
-const list = [apples --> grapes --> pears]
+// const list = [apples --> grapes --> pears]
 
-apples
-8947 --> grapes
-          8742 --> pears
-                    372 --> null
+// apples
+// 8947 --> grapes
+//           8742 --> pears
+//                     372 --> null
+
+// Pointers
+
+let obj1 = { a: true};
+let obj2 = obj1;
+obj1.a = 'Hello john'
+delete obj1
+obj2 = 'hello'
+// console.log('1', obj1)
+console.log('2', obj2)
