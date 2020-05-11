7e1242ebbdc4ac8987b3a075d32f2633bc77471e
dvru <damvaru4@gmail.com>
2020-05-11 14:19:26 -0500

    insert

--- z-t-m Data-Structures/LinkedList/list.js
+++ z-t-m Data-Structures/LinkedList/list.js
@@ -12,7 +12,12 @@
 //         }
 //     }
 // }
-
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 class LinkedList {
     constructor(value) {
         this.head = {
@@ -23,25 +28,35 @@ class LinkedList {
         this.length = 1;
     }
     append(value) {
-    const newNode = {
-        value: value,
-        next: null
-        }
-    // console.log(newNode)
+    const newNode = new Node(value)
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this; // this linkedlist
     }
     prepend(value) { // 1-->10-->5-->16
-        const newNode = {
-            value: value,
-            next: null
-        };
+        const newNode = new Node(value)
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
         return this;
+      }
+      printList() {
+            const array = [];
+            let currentNode = this.head;
+            while(currentNode !== null) {
+                array.push(currentNode.value)
+                currentNode = currentNode.next;
+            }
+            return array;
+        }
+      insert(index, value) {
+    //Check for proper parameters;
+        if(index >= this.length) {
+        console.log('yes')
+        return this.append(value);
+      }
+
       }
 }
 
@@ -50,10 +65,8 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
-console.log(myLinkedList)
-// LinkedList {
-//     head: { value: 10, next: null },
-//     tail: { value: 10, next: null },
-//     length: 1
-//  }
+myLinkedList.insert(200, 99)
+console.log(myLinkedList.printList())
+// console.log(myLinkedList)
+
   
