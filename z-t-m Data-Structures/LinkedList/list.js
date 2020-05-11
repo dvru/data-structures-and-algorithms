d627604467c5dfd170a4454c0f3d884214ec936d
dvru <damvaru4@gmail.com>
2020-05-11 15:17:51 -0500

    reverse

--- z-t-m Data-Structures/LinkedList/list.js
+++ z-t-m Data-Structures/LinkedList/list.js
@@ -84,18 +84,38 @@ class LinkedList {
         this.length--;
         return this.printList();
       }
+      reverse() {
+        if (!this.head.next) {
+          return this.head;
+        }
+        let first = this.head;
+        this.tail = this.head;
+        let second = first.next;
+    
+        while(second) {
+          const temp = second.next;
+          second.next = first;
+          first = second;
+          second = temp;
+        }
+    
+        this.head.next = null;
+        this.head = first;
+        return this.printList();
+      }
 }
 
 const myLinkedList = new LinkedList(10);
-
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
 myLinkedList.insert(2, 99)
 myLinkedList.insert(20, 88)
+myLinkedList.printList()
 myLinkedList.remove(2)
 myLinkedList.remove(2)
-console.log(myLinkedList.printList())
+console.log(myLinkedList.reverse())
+// console.log(myLinkedList.printList())
 // console.log(myLinkedList)
 
   
