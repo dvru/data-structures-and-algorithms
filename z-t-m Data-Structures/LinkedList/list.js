e992e56c50c1788980eb0b4ecd869aac582d95d0
dvru <damvaru4@gmail.com>
2020-05-11 13:49:34 -0500

    created linkedlist

--- z-t-m Data-Structures/LinkedList/list.js
+++ z-t-m Data-Structures/LinkedList/list.js
@@ -1,14 +1,35 @@
-10-->5-->16
+// 10-->5-->16
 
-let myLinkedList = {
-    head: {             // node with value/next prop
-        value: 10,
-        next: {
-            value: 5,
-            next: {
-                value: 16,
-                next: null // null terminated, end of list
-            }
+// let myLinkedList = {
+//     head: {             // node with value/next prop
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null // null terminated, end of list
+//             }
+//         }
+//     }
+// }
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null 
         }
+        this.tail = this.head;
+        this.length = 1;
     }
 }
+
+const myLinkedList = new LinkedList(10);
+
+console.log(myLinkedList)
+// LinkedList {
+//     head: { value: 10, next: null },
+//     tail: { value: 10, next: null },
+//     length: 1
+//  }
+  
