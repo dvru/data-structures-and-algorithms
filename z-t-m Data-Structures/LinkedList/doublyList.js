4151d767e2ecbec011b39306e1a75d72f95bf880
dvru <damvaru4@gmail.com>
2020-05-11 14:56:54 -0500

    doubly linked lists

--- z-t-m Data-Structures/LinkedList/doublyList.js
+++ z-t-m Data-Structures/LinkedList/doublyList.js
@@ -20,25 +20,36 @@ class Node {
         this.next = null;
     }
 }
-class LinkedList {
+class DoublyLinkedList {
     constructor(value) {
         this.head = {
             value: value,
-            next: null 
+            next: null, 
+            prev: null
         }
         this.tail = this.head;
         this.length = 1;
     }
     append(value) {
-    const newNode = new Node(value)
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        }
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this; // this linkedlist
     }
     prepend(value) { // 1-->10-->5-->16
-        const newNode = new Node(value)
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        }
         newNode.next = this.head;
+        this.head.prev = newNode;
         this.head = newNode;
         this.length++;
         return this;
@@ -58,7 +69,11 @@ class LinkedList {
         console.log('yes')
         return this.append(value);
       }
-        const newNode = new Node(value)
+      const newNode = {
+        value: value,
+        next: null,
+        prev: null
+     }
         const leader = this.traverseToIndex(index-1);
         const holdingPointer = leader.next;
         leader.next = newNode;
@@ -86,16 +101,16 @@ class LinkedList {
       }
 }
 
-const myLinkedList = new LinkedList(10);
+const myLinkedList = new DoublyLinkedList(10);
 
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
-myLinkedList.insert(2, 99)
-myLinkedList.insert(20, 88)
-myLinkedList.remove(2)
-myLinkedList.remove(2)
+// myLinkedList.insert(2, 99)
+// myLinkedList.insert(20, 88)
+// myLinkedList.remove(2)
+// // myLinkedList.remove(2)
 console.log(myLinkedList.printList())
-// console.log(myLinkedList)
+console.log(myLinkedList)
 
   
