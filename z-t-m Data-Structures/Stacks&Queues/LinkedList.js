3a3152a77dbe2aad6515634e904abef94107a459
dvru <damvaru4@gmail.com>
2020-06-04 13:29:31 -0500

    exercise

--- z-t-m Data-Structures/Stacks&Queues/LinkedList.js
+++ z-t-m Data-Structures/Stacks&Queues/LinkedList.js
@@ -17,16 +17,37 @@ class Node {
         return this.top
     }
     push(value){ // add to stack
-        const neNode = new Node(value);
-        
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top - newNode;
+            this.top.next = holdingPointer
+        }
+        this.length++;
+        return this;
     }
     pop(){
+        if (this.top) {
+            return null;
+          }
+          const holdingPointer = this.top;
+          this.top = this.top.next;
+          this.length--;
+          return this;
     }
     //isEmpty
   }
   
-  const myStack = new Stack();
-  myStack.peek()
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('Udemy');
+myStack.push('Discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
   
   
   
