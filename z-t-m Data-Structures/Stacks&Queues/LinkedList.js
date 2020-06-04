133180d5348281937b967480ace1b92bf5fcbcd1
dvru <damvaru4@gmail.com>
2020-06-04 13:14:18 -0500

    exercise

--- z-t-m Data-Structures/Stacks&Queues/LinkedList.js
+++ z-t-m Data-Structures/Stacks&Queues/LinkedList.js
@@ -1,1 +1,33 @@
-// Stack Implementation: LinkedLists
+// Stack Exercise: LinkedLists
+
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Stack {
+    constructor(){
+      this.top = null; // top plate
+      this.bottom = null; // bottom plate 
+      this.length = 0;
+    }
+    peek() {
+    }
+    push(value){
+    }
+    pop(){
+    }
+    //isEmpty
+  }
+  
+  const myStack = new Stack();
+  
+  
+  
+  
+  //Discord
+  //Udemy
+  //google
+  
