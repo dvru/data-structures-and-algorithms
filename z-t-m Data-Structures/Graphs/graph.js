67a1833581aaf083476e46224e438e70bac523fb
dvru <damvaru4@gmail.com>
2020-06-05 14:02:39 -0500

    made graph

--- z-t-m Data-Structures/Graphs/graph.js
+++ z-t-m Data-Structures/Graphs/graph.js
@@ -1,13 +1,16 @@
 class Graph { 
     constructor() { 
-      this.numberOfNodes = 0;
-      this.adjacentList = {
-      }; 
+      this.numberOfNodes = 0; 
+      this.adjacentList = {}; 
     } 
     addVertex(node)  { 
+      this.adjacentList[node] = []; 
+      this.numberOfNodes++;
     } 
     addEdge(node1, node2) { 
-      //undirected Graph 
+      //uniderected Graph 
+      this.adjacentList[node1].push(node2); 
+      this.adjacentList[node2].push(node1); 
     } 
     showConnections() { 
       const allNodes = Object.keys(this.adjacentList); 
@@ -23,7 +26,7 @@ class Graph {
   } 
   } 
   
-  const myGraph = new Graph();
+  var myGraph = new Graph();
   myGraph.addVertex('0');
   myGraph.addVertex('1');
   myGraph.addVertex('2');
@@ -41,11 +44,3 @@ class Graph {
   myGraph.addEdge('6', '5');
   
   myGraph.showConnections(); 
-  //Answer:
-  // 0-->1 2 
-  // 1-->3 2 0 
-  // 2-->4 1 0 
-  // 3-->1 4 
-  // 4-->3 2 5 
-  // 5-->4 6 
-  // 6-->5
