bb07a4c62a9a46a5219154f6b1fd6e753702a7ce
dvru <damvaru4@gmail.com>
2020-06-08 16:35:19 -0500

    bubble sort

--- z-t-m Data-Structures/Algorithms/main2-1.js
+++ z-t-m Data-Structures/Algorithms/main2-1.js
@@ -1,4 +1,4 @@
-// bubble sort
+// Bubble sort
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
---
+++ z-t-m Data-Structures/Algorithms/main2-2.js
@@ -0,0 +1,9 @@
+// Selection Sort
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+  
+}
+
+selectionSort(numbers);
