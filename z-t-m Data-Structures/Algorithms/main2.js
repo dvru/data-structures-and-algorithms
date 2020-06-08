49380a01ca907306991dbe79af7b2a50e1d504cd
dvru <damvaru4@gmail.com>
2020-06-08 16:07:23 -0500

    sorting

--- z-t-m Data-Structures/Algorithms/main2.js
+++ z-t-m Data-Structures/Algorithms/main2.js
@@ -1,1 +1,11 @@
 //Algorithms: Sorting
+
+const letters = ['a', 'd', 'e', 'f', 'g', 'c', 'h', 'i', 'b', 'j'];
+const basket = [2, 65, 34, 2, 1, 7, 8];
+
+const spanish = ['']
+// console.log(letters.sort());
+
+console.log(basket.sort(function (a, b) {
+    return a - b;
+}));
