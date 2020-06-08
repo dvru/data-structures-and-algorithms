bdcd4351daf3baad72b110aaac5c76628f97af7b
dvru <damvaru4@gmail.com>
2020-06-08 15:07:10 -0500

    fibonacci

--- z-t-m Data-Structures/Algorithms/main1.js
+++ z-t-m Data-Structures/Algorithms/main1.js
@@ -7,13 +7,21 @@
 
 //For example: fibonacciRecursive(6) should return 8
 
+
 function fibonacciIterative(n){
-    //code here;
+    let arr = [0, 1]; // initial item
+    for (let i = 2; i < n + 1; i++){
+      arr.push(arr[i - 2] + arr[i -1]);
+    }
+   return arr[n];
   }
   fibonacciIterative(3);
   
   function fibonacciRecursive(n) {
-    //code here;
+    if (n < 2){
+      return n;
+    }
+    return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2)
   }
   
-  fibonacciRecursive(3)
+  fibonacciRecursive(10)
