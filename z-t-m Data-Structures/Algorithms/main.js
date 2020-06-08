78542b1cf9b1c9caf16830cc784bc30996fe6f12
dvru <damvaru4@gmail.com>
2020-06-08 14:25:35 -0500

    recursion

--- z-t-m Data-Structures/Algorithms/main.js
+++ z-t-m Data-Structures/Algorithms/main.js
@@ -1,1 +1,12 @@
 //Algorithms: Recursion
+let counter = 0;
+function inception() { 
+    console.log(counter)
+    if (counter > 3) {
+        return 'done!'
+    }
+    counter ++
+    // inception(); 
+}
+
+console.log(inception())
