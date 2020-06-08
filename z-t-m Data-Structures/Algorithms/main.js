3267084724f131229b06995c1dfa5cdec694eaf7
dvru <damvaru4@gmail.com>
2020-06-08 14:30:17 -0500

    recursion

--- z-t-m Data-Structures/Algorithms/main.js
+++ z-t-m Data-Structures/Algorithms/main.js
@@ -1,12 +1,36 @@
 //Algorithms: Recursion
-let counter = 0;
-function inception() { 
-    console.log(counter)
-    if (counter > 3) {
-        return 'done!'
-    }
-    counter ++
-    // inception(); 
-}
+// let counter = 0;
+// function inception() { 
+//     console.log(counter)
+//     if (counter > 3) {
+//         return 'done!'
+//     }
+//     counter ++
+//     // inception(); 
+// }
 
-console.log(inception())
+// console.log(inception())
+
+function findFactorialIterative(number) {
+    let answer = 1;
+    // you actually no longer need the if statement because of the for loop
+    // if (number === 2) {
+    //   answer = 2;
+    // }
+    for (let i = 2; i <= number; i++) {
+      answer = answer * i;
+    }
+    return answer;
+  }
+  
+  function findFactorialRecursive(number) {
+    if(number === 2) {
+      return 2;
+    }
+    return number * findFactorialRecursive(number - 1)
+  }
+  
+  console.log(findFactorialIterative(5))
+  console.log(findFactorialRecursive(5))
+  
+  //If you want, try to add a base case condition for the recursive solution if the parameter given is less than 2
