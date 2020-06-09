35e48e25420ca29aecae316d890f79532dd4a82a
dvru <damvaru4@gmail.com>
2020-06-08 20:03:10 -0500

    Searching

--- z-t-m Data-Structures/Algorithms/main3.js
+++ z-t-m Data-Structures/Algorithms/main3.js
@@ -1,2 +1,17 @@
 //Algorithms: Searching + BFS + DFS
 
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+beasts.indexOf('Godzilla');
+
+beasts.findIndex(function(item){
+  return item === 'Godzilla';
+});
+
+beasts.find(function(item){
+  return item === 'Godzilla';
+})
+
+beasts.includes('Godzilla')
+
+// returns true
