46d48d1798227b70af272da8b2711e512b1c37fe
dvru <damvaru4@gmail.com>
2020-06-09 11:17:15 -0500

    cache

--- z-t-m Data-Structures/Algorithms/DynamicProgramming/main4.js
+++ z-t-m Data-Structures/Algorithms/DynamicProgramming/main4.js
@@ -1,1 +1,20 @@
 //Algorithms: Dynamic Programming
+
+function addTo80(n) {
+    // console.log('long time')
+    return n + 80;
+}
+
+let cache = {};
+function memoizedAddTo80(n) {
+    if(n in cache) {
+        return cache[n]
+    } else {
+        console.log('long time')
+        cache[n] = n + 80
+        return cache[n]
+    }
+}
+
+console.log(memoizedAddTo80(5))
+// console.log(memoizedAddTo80(5))
