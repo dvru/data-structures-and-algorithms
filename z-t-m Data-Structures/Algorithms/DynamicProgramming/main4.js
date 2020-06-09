b771b5bdbb678d4229f9be6ff329d8fab4816195
dvru <damvaru4@gmail.com>
2020-06-09 11:24:36 -0500

    cache

--- z-t-m Data-Structures/Algorithms/DynamicProgramming/main4.js
+++ z-t-m Data-Structures/Algorithms/DynamicProgramming/main4.js
@@ -1,20 +1,48 @@
 //Algorithms: Dynamic Programming
 
+//learn to cache
 function addTo80(n) {
-    // console.log('long time')
     return n + 80;
-}
-
-let cache = {};
-function memoizedAddTo80(n) {
-    if(n in cache) {
-        return cache[n]
+  }
+  
+  addTo80(5)
+  
+  let cache = {};
+  function memoizeAddTo80(n) {
+    if (n in cache) {
+      return cache[n];
     } else {
-        console.log('long time')
-        cache[n] = n + 80
-        return cache[n]
+      console.log('long time');
+      const answer = n + 80;
+      cache[n] = answer;
+      return answer;
     }
-}
-
-console.log('1', memoizedAddTo80(5))
-console.log('2', memoizedAddTo80(5))
+  }
+  
+  // console.log(1, memoizeAddTo80(6))
+  // // console.log(cache)
+  // // console.log('-----------')
+  // console.log(2, memoizeAddTo80(6))
+  
+  // let's make that better with no global scope. 
+  // This is closure in javascript so.
+  function memoizeAddTo80(n) { 
+    let cache = {};
+    return function(n) {
+      if (n in cache) {
+        return cache[n];
+      } else {
+        console.log('long time');
+        const answer = n + 80;
+        cache[n] = answer;
+        return answer;
+      }
+    }
+  }
+  
+  const memoized = memoizeAddTo80();
+  console.log(1, memoized(7))
+  // console.log(cache)
+  // console.log('-----------')
+  console.log(2, memoized(7))
+  
