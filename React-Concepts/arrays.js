02ad5d1d2cb574d6bc39c4def61b014b41aee44c
dvru <damvaru4@gmail.com>
2020-07-13 16:32:58 -0500

    React concepts

--- React-Concepts/arrays.js
+++ React-Concepts/arrays.js
@@ -1,33 +1,37 @@
 // Key React Concepts
 
 map();
-const myArray = [1, 2, 3, 4];
-myArray.map(i => i + 1) // (4) [2, 3, 4, 5]
+    const myArray = [1, 2, 3, 4];
+    myArray.map(i => i + 1) // (4) [2, 3, 4, 5]
+
 
 filter();
-const myArray = [1, 3, 5, 7, 9]
-myArray.filter(i => true) // (5) [1, 3, 5, 7, 9]
-myArray.filter(i => false) // []
-myArray.filter(i => i > 4) // (3) [5, 7, 9]
+    const myArray = [1, 3, 5, 7, 9]
+    myArray.filter(i => true) // (5) [1, 3, 5, 7, 9]
+    myArray.filter(i => false) // []
+    myArray.filter(i => i > 4) // (3) [5, 7, 9]
+
 
 reduce();
+array.reduce((accumulator, i) => accumulator + i, 0) // 15
+array.reduce((accumulator, i) => accumulator + i, 10) // 25
 
 
 find();
-const myArray = [1, 3, 5, 7, 9]
-myArray.find(i => i === 5) //5
-myArray.find(i => i > 4) //5
+    const myArray = [1, 3, 5, 7, 9]
+    myArray.find(i => i === 5) //5
+    myArray.find(i => i > 4) //5
 
-const peopleArray = [{id: 1}, {id: 4},{id: 7}] //undefined
-peopleArray.find(i => i.id === 4) //{id: 4}
+    const peopleArray = [{id: 1}, {id: 4},{id: 7}] //undefined
+    peopleArray.find(i => i.id === 4) //{id: 4}
 
 
 includes();
-const newArray = [{id: 1}, {id: 2}, {id: 3}]
-newArray[0] // {id: 1}
-newArray[2] //{id: 3}
+    const newArray = [{id: 1}, {id: 2}, {id: 3}]
+    newArray[0] // {id: 1}
+    newArray[2] //{id: 3}
 
-const myArray = [1, 2, 3, 4, 5];
-myArray.includes(2, 3) //false
-myArray.includes(2, 2) // false
-myArray.includes(2, 1)  // true
+    const myArray = [1, 2, 3, 4, 5];
+    myArray.includes(2, 3) //false
+    myArray.includes(2, 2) // false
+    myArray.includes(2, 1)  // true
